561359b3a50902ab4405b24bf18533e770c1c539
tmcdonough45 <tjmcdonough45@gmail.com>
2019-01-08 18:17:23 +0000

    Add in database settings for MySQL as comment. Disable submit button for bracket entry until after first elimination.

--- BracketHub/BracketHub/settings/prod.py
+++ BracketHub/BracketHub/settings/prod.py
@@ -7,4 +7,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'prod.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'tjmcdonough45$bracketracker',
+    #     'USER': 'tjmcdonough45',
+    #     'PASSWORD': 'hungryHungryhippos69',
+    #     'HOST': 'tjmcdonough45.mysql.pythonanywhere-services.coms',
+    # }
 }
